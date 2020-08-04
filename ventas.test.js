b504c7548b948aba227ac84b569e06fc11a86ba3
Maria Alarcon <mariagaa993@gmail.com>
2020-08-04 15:59:29 -0400

    ejercicio 3

--- ventas.test.js
+++ ventas.test.js
@@ -1,3 +1,8 @@
 /* Con esta variable estamos importando las funciones
 y arrays del archivo ventas.js (para no duplicar código) */
 const functionsYArrays = require('./ventas');
+
+// Test del método 3 ------------------------------
+test('Test 3: Ingreso el nombre de una vendedora y retorno el importe de ventas', () => {
+    expect(functionsYArrays.ventasVendedora("Grace")).toBe(990);
+});
