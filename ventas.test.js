66784f02a1a69c7aa4543a6e3bf142747ea79323
Maria Alarcon <mariagaa993@gmail.com>
2020-08-18 12:59:59 -0400

    agregando ejercicio 4

--- ventas.test.js
+++ ventas.test.js
@@ -2,39 +2,39 @@ const functionsYArrays = require('./ventas')
 
 // Test de validaciones ------------------------------
 describe('Validando los errores', () => {
-    test('Vendedora: Ingreso nombre incorrecto y tira error',() => {
-        expect(()=>functionsYArrays.validarVendedora('Shakira')).toThrow('La vendedora no existe');
+    test('Vendedora: Ingreso nombre incorrecto y tira error', () => {
+        expect(() => functionsYArrays.validarVendedora('Shakira')).toThrow('La vendedora no existe');
     });
-    test('Sucursal: Ingreso sucursal incorrecta y tira error',() => {
-        expect(()=>functionsYArrays.validarSucursal('Caribe')).toThrow('Sucursal invalida');
+    test('Sucursal: Ingreso sucursal incorrecta y tira error', () => {
+        expect(() => functionsYArrays.validarSucursal('Caribe')).toThrow('Sucursal invalida');
     });
-    test('Componente: Ingreso componente incorrecto y tira error',() => {
-        expect(()=>functionsYArrays.validarComponente('Tuerca')).toThrow('El componente no existe');
+    test('Componente: Ingreso componente incorrecto y tira error', () => {
+        expect(() => functionsYArrays.validarComponente('Tuerca')).toThrow('El componente no existe');
     });
 });
 
 
 // Test del método 1 ------------------------------
 describe('Validando el método 1', () => {
-  test('Test 1.1: No ingreso ningún componente, por lo que, debería retornar 0', () => {
-	  expect(functionsYArrays.precioMaquina([])).toEqual(0)
-  });
-  test('Test 1.2: Ingreso un componente y debería retornar su precio', () => {
-	  expect(functionsYArrays.precioMaquina(['Motherboard ASUS 1200'])).toEqual(100)
-  });
-  test('Test 3: Ingreso varios componentes, y me debería retornar el valor de su suma', () => {
-	  expect(functionsYArrays.precioMaquina([
-		  'Motherboard ASUS 1200',
+    test('Test 1.1: No ingreso ningún componente, por lo que, debería retornar 0', () => {
+	    expect(functionsYArrays.precioMaquina([])).toEqual(0)
+    });
+    test('Test 1.2: Ingreso un componente y debería retornar su precio', () => {
+	    expect(functionsYArrays.precioMaquina(['Motherboard ASUS 1200'])).toEqual(100)
+    });
+    test('Test 1.3: Ingreso varios componentes, y me debería retornar el valor de su suma', () => {
+	    expect(functionsYArrays.precioMaquina([
+		    'Motherboard ASUS 1200',
 			'HDD Toyiva',
 			'RAM Quinston Fury',
 			'Monitor ASC 543',
 		])).toEqual(670)
-  });
-};
+    });
+});
 
 
 // Test del método 2 ------------------------------
-test('Test 2: Ingreso un componente y retorno la cantidad de unidades vendidas', () =>{
+test('Test 2: Ingreso un componente y retorno la cantidad de unidades vendidas', () => {
     expect(functionsYArrays.cantidadVentasComponente('HDD Toyiva')).toBe(1);
 });
 
@@ -42,6 +42,12 @@ test('Test 2: Ingreso un componente y retorno la cantidad de unidades vendidas',
 // Test del método 3 ------------------------------
 test('Test 3: Ingreso el nombre de una vendedora y retorno el importe de ventas', () => {
     expect(functionsYArrays.ventasVendedora('Grace')).toBe(990);
+});
+
+
+// Test del método 4 ------------------------------
+test('Test 4: Saber cuál es el componente más vendido', () => {
+    expect(functionsYArrays.componenteMasVendido()).toBe("Monitor GPRS 3000");
 });
 
 
